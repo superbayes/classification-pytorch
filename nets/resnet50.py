fa2dd7a49939fb6651a4d3a376693e8354d1d113
superbayes <56495942+superbayes@users.noreply.github.com>
2022-05-02 10:57:18 +0800

    Update resnet50.py
    from torch.hub import load_state_dict_from_url

--- nets/resnet50.py
+++ nets/resnet50.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
-from torchvision.models.utils import load_state_dict_from_url
+# from torchvision.models.utils import load_state_dict_from_url
+from torch.hub import load_state_dict_from_url
 
 model_urls = {
     'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
